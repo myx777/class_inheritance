8470ba83f3bd32cfebf058e95ddfd07f48f1fd3e
MYX <it.anton22@gmail.com>
2023-10-07 15:42:17 +0300

    optimised code of class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 class Character {
-    constructor(name, type, health, level) {
+    constructor(name, type) {
         this.name = this.characterName(name);
         this.type = this.typeName(type);
         this.health = 100;
@@ -29,45 +29,45 @@ class Character {
 }
 
 class Bowerman extends Character {
-    constructor(name, type, health, level, attack, defence){
-        super(name, type, health, level, attack, defence);
+    constructor(name){
+        super(name, 'Bowman');
         this.attack = 25;
         this.defence = 25;
     }
 }
 
 class Swordsman extends Character {
-    constructor(name, type, health, level, attack, defence){
-        super(name, type, health, level, attack, defence);
+    constructor(name){
+        super(name, 'Swordsman');
         this.attack = 40;
         this.defence = 10;
     }
 }
 class Magician extends Character {
-    constructor(name, type, health, level, attack, defence){
-        super(name, type, health, level, attack, defence);
+    constructor(name){
+        super(name, 'Magician');
         this.attack = 10;
         this.defence = 40;
 
     }
 }
 class Daemon extends Character {
-    constructor(name, type, health, level, attack, defence){
-        super(name, type, health, level, attack, defence);
+    constructor(name){
+        super(name, 'Daemon');
         this.attack = 25;
         this.defence = 25;
     }
 }
 class Undead extends Character {
-    constructor(name, type, health, level, attack, defence){
-        super(name, type, health, level, attack, defence);
+    constructor(name){
+        super(name, 'Undead');
         this.attack = 40;
         this.defence = 10;
     }
 }
 class Zombie extends Character {
-    constructor(name, type, health, level, attack, defence){
-        super(name, type, health, level, attack, defence);
+    constructor(name){
+        super(name, 'Zombie');
         this.attack = 10;
         this.defence = 40;
     }
